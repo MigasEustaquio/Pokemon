33f7286d8a9bed6e083a69444ec5fee3832153ca
MigasEustaquio <31831761+MigasEustaquio@users.noreply.github.com>
2017-09-18 20:32:21 -0400

    Add files via upload

---
+++ arq.h
@@ -0,0 +1,126 @@
+void ler(CARD *temp, int i){
+
+	int j;
+	char x[100], y[3];
+
+	FILE *arquivo;
+	arquivo=fopen("pokemons.txt", "r");
+
+	fseek(arquivo, 0, 0);
+
+	for(j=1;j<i;j++){
+		
+		fscanf(arquivo, "%[^\n]s", x);
+		fscanf(arquivo, "%[^.]s", x);
+	}
+
+	fseek(arquivo, 1, 1);
+	
+	fscanf(arquivo, "%[^;]s", temp->nome);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->tipo);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->weak);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->resis);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->num=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->hp=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->cost=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->atk1=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->atk2=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->dmg1=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->dmg2=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->effect1=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->effect2=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->nivel=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->evolucao=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", y);
+	temp->power=atoi(y);
+	fseek(arquivo, 1, 1);
+
+	fclose(arquivo);
+}
+
+void lere(ENERGY *tempe, int i){
+
+	int j=1;
+	char x[20];
+
+	FILE *arquivo;
+	arquivo=fopen("energy.txt", "r");
+
+	fseek(arquivo, 0, 0);
+	
+	for(j=1;j<i;j++){
+		
+		fscanf(arquivo, "%[^\n]s", x);
+		fscanf(arquivo, "%[^.]s", x);
+		fseek(arquivo, 1, 1);
+	}
+		
+	fscanf(arquivo, "%[^;]s", tempe->nome);
+	tempe->num=i;
+
+	fclose(arquivo);
+}
+
+void lert(TRAINER *tempt, int i){
+
+	int j=1;
+	char x[20];
+
+	FILE *arquivo;
+	arquivo=fopen("trainer.txt", "r");
+
+	fseek(arquivo, 0, 0);
+	
+	for(j=1;j<i;j++){
+		
+		fscanf(arquivo, "%[^\n]s", x);
+		fscanf(arquivo, "%[^.]s", x);
+		fseek(arquivo, 1, 1);
+	}
+		
+	fscanf(arquivo, "%[^;]s", tempt->nome);
+	tempt->num=i;
+
+	fclose(arquivo);
+}
---
+++ struct.h
@@ -0,0 +1,30 @@
+typedef struct card{
+
+	int num, hp, cost;
+	int atk1, atk2, dmg1, dmg2;
+	int effect1, effect2;
+	int nivel, evolucao, power;
+	char nome[20], tipo[10], weak[10], resis[10];
+
+}CARD;
+
+typedef struct energy{
+
+	int num;
+	char nome[20];
+
+}ENERGY;
+
+typedef struct trainer{
+
+	int num;
+	char nome[20];
+
+}TRAINER;
+
+typedef struct mesa{
+
+	CARD ativa, banco1, banco2, banco3, banco4;
+
+}MESA;
+
