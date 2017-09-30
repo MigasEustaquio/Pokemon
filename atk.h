d39f28f328c002a5b0c98dd00d9e93524e7543b0
MigasEustaquio <31831761+MigasEustaquio@users.noreply.github.com>
2017-09-30 18:19:30 -0400

    Update atk.h

--- atk.h
+++ atk.h
@@ -2,6 +2,9 @@ void teste(CARD *temp);
 void substituir(MESA *mesa, int x);
 void imprimirMesa(MESA *mesa, int k);
 int energ(CARD *card, int j);
+int energ2(CARD *card);
+void troca(CARD *card1, CARD *card2, CARD *temp, int q);
+void consertar(CARD *card);
 
 void atk(MESA *mesa1, MESA *mesa2){
 
@@ -104,96 +107,134 @@ void atk(MESA *mesa1, MESA *mesa2){
 
 void substituir(MESA *mesa, int x){
 
-	int i=0;
-	CARD *aux = NULL;
+	int i=0, j, m=0;
 
 	imprimirMesa(mesa, 1);
+
+	if(mesa->banco[0]==NULL){
+		printf("\nNenhum pokemon no Banco.\n\n");
+		return;
+	}
 
 	printf("\nQual Pokemon do Banco voce deseja tornar ativo?\n");
 	scanf("%d", &i);
 
-	if(x==0){
-		CARD *aux = (CARD *) malloc(sizeof(CARD));
-		aux=mesa->ativo;
-	}
+//	j=energ2(mesa->ativo);
+
+//	if(j==0){
+//		printf("\nO pokemon nao possui a energia necessaria para recuar.\n\n");
+//		return;
+//	}
+	CARD *aux = (CARD *) malloc(sizeof(CARD));
+	*aux=*mesa->ativo;
 
 	if(x==1)
 		mesa->cont--;
-
-	mesa->ativo=mesa->banco[i-1];
 
 	switch(i){
 
 		case 4:
 
-			if(x==1)
+			if(x==1){
+				troca(mesa->banco[3], mesa->ativo, aux, 2);
 				mesa->banco[3]=NULL;
-			else
-				mesa->banco[3]=aux;
-			return;
+			}
+			else{
+				troca(mesa->banco[3], mesa->ativo, aux, 2);
+				consertar(mesa->banco[m]);
+			}
 			break;
-
 		case 3:
 
-			if(x==1){
-				if(mesa->banco[3]!=NULL)
-					mesa->banco[2]=mesa->banco[3];
-				else
-					mesa->banco[2]=NULL;
-
-				mesa->banco[3]=NULL;
+			if(mesa->banco[3]!=NULL){
+				troca(mesa->banco[3], mesa->banco[2], aux, 2);
+				troca(mesa->banco[3], mesa->ativo, aux, 2);
+				consertar(mesa->banco[3]);
+				consertar(mesa->banco[2]);
+				consertar(mesa->banco[1]);
+				consertar(mesa->banco[0]);
+				if(x==1)
+					mesa->banco[3]=NULL;
 			}
-			else
-				mesa->banco[2]=aux;
-
-			return;
+			else{
+				troca(mesa->banco[2], mesa->ativo, aux, 2);
+				consertar(mesa->banco[2]);
+				consertar(mesa->banco[1]);
+				consertar(mesa->banco[0]);
+				if(x==1)
+					mesa->banco[2]=NULL;
+			}
 			break;
-
 		case 2:
 
-			if(x==1){
-				if(mesa->banco[2]!=NULL)
-					mesa->banco[1]=mesa->banco[2];
-				else
-					mesa->banco[1]=NULL;
+			if(mesa->banco[2]!=NULL){
 
-				if(mesa->banco[3]!=NULL)
-					mesa->banco[2]=mesa->banco[3];
-				else
-					mesa->banco[2]=NULL;
-
-				mesa->banco[3]=NULL;
+				troca(mesa->banco[2], mesa->banco[1], aux, 2);
+				if(mesa->banco[3]!=NULL){
+					troca(mesa->banco[3], mesa->banco[2], aux, 2);
+					troca(mesa->banco[3], mesa->ativo, aux, 2);
+					consertar(mesa->banco[3]);
+					consertar(mesa->banco[2]);
+					consertar(mesa->banco[1]);
+					consertar(mesa->banco[0]);
+					if(x==1)
+						mesa->banco[3]=NULL;
+				}else{
+					troca(mesa->banco[2], mesa->ativo, aux, 2);
+					consertar(mesa->banco[2]);
+					consertar(mesa->banco[1]);
+					consertar(mesa->banco[0]);
+					if(x==1)
+						mesa->banco[2]=NULL;
+				}
 			}
-			else
-				mesa->banco[1]=aux;
-			return;
+			else{
+				troca(mesa->banco[1], mesa->ativo, aux, 2);
+				consertar(mesa->banco[1]);
+				consertar(mesa->banco[0]);
+				if(x==1)
+					mesa->banco[1]=NULL;
+			}
 			break;
-
 		case 1:
 
-			if(x==1){
-				if(mesa->banco[1]!=NULL)
-					mesa->banco[0]=mesa->banco[1];
-				else
+			if(mesa->banco[1]!=NULL){
+				troca(mesa->banco[1], mesa->banco[0], aux, 2);
+				if(mesa->banco[2]!=NULL){
+					troca(mesa->banco[2], mesa->banco[1], aux, 2);
+					if(mesa->banco[3]!=NULL){
+						troca(mesa->banco[3], mesa->banco[2], aux, 2);
+						troca(mesa->banco[3], mesa->ativo, aux, 2);
+						consertar(mesa->banco[3]);
+						consertar(mesa->banco[2]);
+						consertar(mesa->banco[1]);
+						consertar(mesa->banco[0]);
+						if(x==1)
+							mesa->banco[3]=NULL;
+					}else{
+						troca(mesa->banco[2], mesa->ativo, aux, 2);
+						consertar(mesa->banco[2]);
+						consertar(mesa->banco[1]);
+						consertar(mesa->banco[0]);
+						if(x==1)
+							mesa->banco[2]=NULL;
+					}
+				}else{
+					troca(mesa->banco[1], mesa->ativo, aux, 2);
+					consertar(mesa->banco[1]);
+					consertar(mesa->banco[0]);
+					if(x==1)
+						mesa->banco[1]=NULL;
+				}
+			}else{
+				troca(mesa->banco[0], mesa->ativo, aux, 2);
+				consertar(mesa->banco[0]);
+				if(x==1)
 					mesa->banco[0]=NULL;
-
-				if(mesa->banco[2]!=NULL)
-					mesa->banco[1]=mesa->banco[2];
-				else
-					mesa->banco[1]=NULL;
-
-				if(mesa->banco[3]!=NULL)
-					mesa->banco[2]=mesa->banco[3];
-				else
-					mesa->banco[2]=NULL;
-
-				mesa->banco[3]=NULL;
 			}
-			else
-				mesa->banco[0]=aux;
-			return;
 			break;
 	}
+	consertar(mesa->ativo);
 }
 
 int energ(CARD *card, int j){
@@ -220,3 +261,122 @@ int energ(CARD *card, int j){
 
 	return 1;
 }
+
+int energ2(CARD *card){
+
+	int energy, energy1, energy2, temp;
+
+	energy=card->energy;
+
+	energy1=energy/10;
+	energy2=energy%10;
+
+	if(energy2+energy1<card->cost)
+		return 0;
+
+	temp=card->cost;
+
+	do{
+		if(energy2>=card->cost){
+			energy2--;
+			temp--;
+		}else
+			if(energy1>=card->cost){
+				energy1--;
+				temp--;
+			}
+	}while(temp>0);
+
+	card->energy=(energy1*10+energy2);
+	return 1;
+}
+
+void troca(CARD *card1, CARD *card2, CARD *temp, int q){
+
+	if(q==2){
+		temp->energy=card1->energy;
+		temp->num=card1->num;
+		temp->hp=card1->hp;
+		temp->cost=card1->cost;
+		temp->atk1=card1->atk1;
+		temp->atk2=card1->atk2;
+		temp->dmg1=card1->dmg1;
+		temp->dmg2=card1->dmg2;
+		temp->effect1=card1->effect1;
+		temp->effect2=card1->effect2;
+		temp->nivel=card1->nivel;
+		temp->evolucao=card1->evolucao;
+		temp->power=card1->power;
+	}
+
+	card1->energy=card2->energy;
+	card1->num=card2->num;
+	card1->hp=card2->hp;
+	card1->cost=card2->cost;
+	card1->atk1=card2->atk1;
+	card1->atk2=card2->atk2;
+	card1->dmg1=card2->dmg1;
+	card1->dmg2=card2->dmg2;
+	card1->effect1=card2->effect1;
+	card1->effect2=card2->effect2;
+	card1->nivel=card2->nivel;
+	card1->evolucao=card2->evolucao;
+	card1->power=card2->power;
+
+	if(q==2){
+		card2->energy=temp->energy;
+		card2->num=temp->num;
+		card2->hp=temp->hp;
+		card2->cost=temp->cost;
+		card2->atk1=temp->atk1;
+		card2->atk2=temp->atk2;
+		card2->dmg1=temp->dmg1;
+		card2->dmg2=temp->dmg2;
+		card2->effect1=temp->effect1;
+		card2->effect2=temp->effect2;
+		card2->nivel=temp->nivel;
+		card2->evolucao=temp->evolucao;
+		card2->power=temp->power;
+	}
+}
+
+void consertar(CARD *temp){
+
+	int j, i=temp->num;
+	char x[100];
+
+	FILE *arquivo;
+	arquivo=fopen("pokemons.txt", "r");
+
+	fseek(arquivo, 0, 0);
+
+	for(j=1;j<i;j++){
+		
+		fscanf(arquivo, "%[^\n]s", x);
+		fscanf(arquivo, "%[^.]s", x);
+	}
+
+	fseek(arquivo, 1, 1);
+	
+	fscanf(arquivo, "%[^;]s", temp->nome);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->tipo);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->weak);
+	fseek(arquivo, 1, 1);
+
+	fscanf(arquivo, " %[^;]s", temp->resis);
+	fseek(arquivo, 1, 1);
+
+	fclose(arquivo);
+}
+
+
+
+
+
+
+
+
