e81de0b8853c7680b54d100259e4b41e5b556809
MigasEustaquio <31831761+MigasEustaquio@users.noreply.github.com>
2017-10-26 16:49:08 -0200

    Update struct.h

--- struct.h
+++ struct.h
@@ -3,7 +3,7 @@ typedef struct card{
 	int num, hp, cost, energy;
 	int atk1, atk2, dmg1, dmg2;
 	int effect1, effect2;
-	int nivel, evolucao, power;
+	int nivel, evolucao, power, status;
 	char nome[20], tipo[10], weak[10], resis[10];
 
 }CARD;
